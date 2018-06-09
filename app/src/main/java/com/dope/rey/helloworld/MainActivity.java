504d46f892f9f730f14532a3b0ba7c35b2d6b17e
Rey Humphrey <reyhumphrey1@gmail.com>
2018-06-09 12:53:20 +0300

    Changed the text to be displayed

--- app/src/main/java/com/dope/rey/helloworld/MainActivity.java
+++ app/src/main/java/com/dope/rey/helloworld/MainActivity.java
@@ -2,6 +2,8 @@ package com.dope.rey.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         //comments
+    }
+
+    public void doIt(View v){
+        Toast.makeText(getApplicationContext(), "Hello Alc 3.0", Toast.LENGTH_LONG).show();
     }
 }
